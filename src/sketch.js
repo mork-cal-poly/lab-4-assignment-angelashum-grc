b59c0db497621677827576a1582606a024bcf843
Angela Shum <angelashum24@gmail.com>
2024-03-03 03:57:25 -0800

    Added Lab 2 Creature Code

--- src/sketch.js
+++ src/sketch.js
@@ -123,4 +123,158 @@ function draw() {
   pop();
   
 }
-        
+
+let clicked = false;
+
+function setup() {
+  createCanvas(400, 400);
+  background(72, 202, 228); //background color 
+}
+
+function draw() {
+
+  //_________ Background x, y, floor
+  drawBackground(0,0,color(202, 240, 248),color(255))
+  
+  //_______________Penguin________________
+    //x,y,colorDark,colorLight,colorEye,colorBeak
+     drawPenguin(200,400,color(29,53,87),color(241,233,218),color(0),color(241,136,5))
+  
+  //________surprise_________
+  if(clicked){
+    //x,y,colorHead,colorEyes,colorNose,colorMouth
+    drawSurprise(200,400,color(29,53,87),color(0),color(241,136,5),color(191,67,66),color(72, 202, 228));
+   }
+  
+}
+//_________________Draw Background____________
+
+function drawBackground(x,y,colorFloor,colorSnow){
+  push();
+    //—Floor
+    translate(x,y)
+    fill(colorFloor)
+    noStroke();
+    rect(0, 300, 400, 100) 
+   pop();
+
+  // —-Snow
+  push();
+    translate(200,400);
+    fill(colorSnow);
+    noStroke();
+    ellipse(-125,-300,10,10);
+    ellipse(-140,-100,10,10);
+    ellipse(-175,-225,10,10);
+    ellipse(110,-125,10,10);
+    ellipse(175,-150,10,10);
+    ellipse(-155,-75,10,10);
+    ellipse(-125,-30,10,10);
+    ellipse(120,-375,10,10);
+    ellipse(-175,-375,10,10);
+    ellipse(125,-300,10,10);
+    ellipse(150,-200,10,10);
+    ellipse(-25,-375,10,10);
+    ellipse(160,-250,10,10);
+    ellipse(150,-25,10,10);
+    ellipse(170,-75,10,10);
+    ellipse(25,-375,10,10);
+  pop();
+}
+
+//_________________Function for Penguin___________________
+function drawPenguin(x,y,colorDark,colorLight,colorEye,colorBeak){
+
+  push();
+    translate(x, y);
+    noStroke();
+  
+  //---Body----
+    fill(colorDark);
+    quad(-75,-225,75,-225,100,-50,-100,-50);
+  
+  //----wings----
+  //left
+  push();
+    translate(-100,-175);
+    fill(colorDark);
+    rotate(PI/4);
+    ellipse(0,0,50,150);
+  pop();
+  //right
+  push();
+    translate(100,-175);
+    fill(colorDark);
+    rotate(3*PI/4);
+    ellipse(0,0,50,150);
+  pop();
+ 
+  //---Head----
+  fill(colorDark);
+  ellipse(0,-275,150);
+  
+  //---Regular Face----
+  fill(colorLight);
+  arc(0,-262.5,117,120,7*PI/4,5*PI/4);
+  ellipse(-30,-262.5,60,90)
+  ellipse(30,-262.5,60,90)
+  //beak
+  fill(colorBeak)
+  quad(-20,-275,0,-280,20,-275,0,-260);
+  //eye
+  fill(colorEye);
+  ellipse(-30,-287.5,10,10)
+  ellipse(30,-287.5,10,10)
+
+  //----Body Circle
+  fill(colorLight);
+  ellipse(0,-150,137.5,210);
+  
+  //----feet----
+  fill(colorBeak);
+  triangle(-50,-100,-25,-50,-75,-50);
+  triangle(50,-100,75,-50,25,-50)
+  arc(-50,-62.5,75,75,7*PI/4,5*PI/4);
+  arc(50,-62.5,75,75,7*PI/4,5*PI/4); 
+  
+  
+  pop();
+  
+}
+
+//________________Surprise________________
+  function drawSurprise(x,y,colorHead,colorEyes,colorNose,colorMouth,colorTears){
+    translate(x,y);
+    //---head---
+    fill(colorHead);
+    noStroke();
+    ellipse(0,-275,150);
+    //---eyes---
+    fill(255);
+    ellipse(-30,-287.5,40,25);
+    ellipse(30,-287.5,40,25);
+    fill(colorEyes);
+    ellipse(-30,-287.5,15,15);
+    ellipse(30,-287.5,15,15);
+    //--nose---
+    fill(colorNose);
+    quad(-20,-275,0,-280,20,-275,0,-240);
+    //---mouth---
+    fill(colorMouth)
+    ellipse(0,-230,40,15);
+    fill(colorTears)
+    rect(-35,-280,10,80);
+    rect(25,-280,10,80);
+  }
+  
+  function mouseClicked() 
+  //---bounding box
+{
+  if(mouseX < 275 &&
+     mouseX > 125 &&
+    mouseY < 200 &&
+     mouseY > 50
+    ) {
+  clicked = !clicked;     
+  }
+  }
